5ecfcad0053461f275b9646e627e26d98e8e4146
Jun Saitou <saitouatusi9415@gmail.com>
2019-03-21 19:28:18 +0900

    add ex1_9

---
+++ Go_book/ex1_9.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	http_pre := "http://"
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, http_pre) {
+			url = http_pre + url
+		}
+		resp, err := http.Get(url)
+		fmt.Println(resp.Status)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
